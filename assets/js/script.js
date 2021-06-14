4689147274a90b1d7fd30e304ca130afd8e86f6b
Ade Mahendra <hendraadem@gmail.com>
2021-06-14 16:10:53 +0700

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// window.onscroll = () => {
+//   let projectAreaOffsetTop = document.getElementById("projects").offsetTop;
+//   let testimonialAreaOffsetTop =
+//     document.getElementById("testimonial").offsetTop;
+
+//   let filterComponent = document.getElementById("project-filter");
+
+//   if (window.scrollY >= projectAreaOffsetTop - 100) {
+//     filterComponent.style.display = "flex";
+//   }
+
+//   if (window.scrollY >= testimonialAreaOffsetTop - 100) {
+//     filterComponent.style.display = "none";
+//   } else {
+//     filterComponent.style.display = "none";
+//   }
+// };
